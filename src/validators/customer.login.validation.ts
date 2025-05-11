ab63405cf98ca505f434a8dccce1a32b66fd8064
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-05-11 19:28:16 +0000

    validation for customer login functionality

---
+++ src/validators/customer.login.validation.ts
@@ -0,0 +1,16 @@
+import {check} from "express-validator"
+
+export const customerLoginValidation = [
+    check('email')
+    .notEmpty()
+    .withMessage('email field must not be empty')
+    .isString()
+    .isEmail()
+    .withMessage('Provide a valid email'),
+
+    check('password')
+    .notEmpty()
+    .withMessage('password field must not be empty')
+    .isString()
+
+]
