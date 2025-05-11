7348b85eeec68c57b4370751135bffd8af06eb2c
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-05-11 19:20:16 +0000

    modfied signup route for customer by adding validator middlewares

--- src/routes/customer.routes.ts
+++ src/routes/customer.routes.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { login, signUp } from "../controllers/customer.controller";
+import { customerSignUpValidation } from "../validators/customer.signup.validator";
+import { validate } from "../validators/validateFields";
 
 const router = express.Router();
 
-router.post('/signup', signUp);
+router.post('/signup',customerSignUpValidation , validate ,signUp);
 router.post('/login', login);
 
 export default router;
