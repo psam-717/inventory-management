3aaf52575259280fa163983bcb890355e56d3fb6
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-05-11 19:27:01 +0000

    included validation middlewares to the customer login route

--- src/routes/customer.routes.ts
+++ src/routes/customer.routes.ts
@@ -2,10 +2,11 @@ import express from "express";
 import { login, signUp } from "../controllers/customer.controller";
 import { customerSignUpValidation } from "../validators/customer.signup.validator";
 import { validate } from "../validators/validateFields";
+import { customerLoginValidation } from "../validators/customer.login.validation";
 
 const router = express.Router();
 
 router.post('/signup',customerSignUpValidation , validate ,signUp);
-router.post('/login', login);
+router.post('/login', customerLoginValidation,validate,login);
 
 export default router;
