1b4627b935e37f11b514763be2b73443b5ebda34
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-05-21 20:26:23 +0000

    included routes for retrieving the data of a logged in user

--- src/routes/customer.routes.ts
+++ src/routes/customer.routes.ts
@@ -1,12 +1,16 @@
 import express from "express";
-import { login, signUp } from "../controllers/customer.controller";
+import { getMyData, login, signUp } from "../controllers/customer.controller";
 import { customerSignUpValidation } from "../validators/customer.signup.validator";
 import { validate } from "../validators/validateFields";
-import { customerLoginValidation } from "../validators/customer.login.validation";
+import { loginValidation } from "../validators/customer.login.validation";
+import { authorization } from "../middlewares/authentication";
+import { purchaseItem } from "../controllers/order.controller";
 
 const router = express.Router();
 
 router.post('/signup',customerSignUpValidation , validate ,signUp);
-router.post('/login', customerLoginValidation,validate,login);
+router.post('/login', loginValidation,validate,login);
+router.post('/purchase', authorization, purchaseItem);
+router.get('/me', authorization, getMyData);
 
 export default router;
