7c19267563f4fcaabfe7c40afd7782593c92b53c
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-05-26 00:23:05 +0000

    specified routes for increasing stock

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,17 +1,28 @@
 import express from "express"
-import { addNewItem, deleteProduct, displayAllProduct, getItemByQuery, getSingleProduct, login, signUp, } from "../controllers/admin.controller";
+import {
+        addNewItem, 
+        addToStock, 
+        deleteProduct, 
+        displayAllProduct, 
+        getItemByQuery, 
+        getSingleProduct, 
+        login, 
+        signUp, 
+    } from "../controllers/admin.controller";
 import { validate } from "../validators/validateFields";
 import { addItemValidation } from "../validators/addItem.validation";
 import { adminSignUpValidation } from "../validators/admin.signUp.validation";
 import { loginValidation } from "../validators/customer.login.validation";
 import { authorization } from "../middlewares/authentication";
 import { isAdmin } from "../middlewares/admin.auth";
+import { addToStockValidation } from "../validators/addToStock.validation";
 
 const router = express.Router();
 
 router.post('/signup', adminSignUpValidation, validate,signUp);
 router.post('/login', loginValidation, validate, login)
 router.post('/add-item',  addItemValidation, validate, addNewItem);
+router.put('/add-to-Stock', addToStockValidation, validate , authorization, isAdmin, addToStock)
 router.get('/get-all-items', authorization, isAdmin ,displayAllProduct)
 router.get('/get-single-item/:id', authorization, isAdmin,getSingleProduct)
 router.delete('/delete-item/:id', authorization, isAdmin,deleteProduct);
