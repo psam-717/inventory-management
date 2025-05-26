54bc5beec37933c8588fff6421c1b0d130e50ed2
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-05-26 00:25:19 +0000

    included field validation for the name

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -147,6 +147,10 @@ export const addToStock = async(req: Request, res: Response): Promise<void> => {
             res.status(404).json({success: false, message: 'product not found'});
             return;
         }
+
+        if(!name || typeof name !== 'string'){
+            res.status(400).json({success: false, message: 'name of product must be a valid and not empty'})
+        }
     
         // make sure quantity specified is an integer
         if(typeof quantity !== 'number' || quantity < 0){
@@ -166,7 +170,7 @@ export const addToStock = async(req: Request, res: Response): Promise<void> => {
         );
 
         if (!updatedStock){
-            res.status(401).json({success: false, message: 'Failed to update stock'});
+            res.status(404).json({success: false, message: 'Failed to update stock'});
             return;
         }
 
